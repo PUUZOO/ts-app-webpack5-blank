b96c705a26f636a04c0e6ff16b82c644ad89dcf4
puuzoo <stanislav.stru@mail.ru>
2021-07-26 17:49:45 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+// Library
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Component
+import MainChat from "@/components/MainChat";
+
+ReactDOM.render(
+  <MainChat />,
+
+  document.getElementById("root")
+);
